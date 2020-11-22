5c024dd042503b8a61d5c590769b7258959d8799
Lucas Braune <lucasbraune@gmail.com>
2020-11-22 19:44:04 -0300

    Move Time Controller to Simulation source files

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,4 +1,39 @@
 #include "simulation.hpp"
+#include <thread>
+
+using std::chrono::milliseconds;
+
+TimeController::TimeController(int targetFps, double playbackSpeed) :
+  sim_start_(high_resolution_clock::now()),
+  fps_(targetFps),
+  playbackSpeed_(playbackSpeed)
+{}
+
+TimeController::TimeController(int targetFps) :
+  TimeController(targetFps, 1.0)
+{}
+
+void TimeController::setStartOfSimulation()
+{
+  sim_start_ = high_resolution_clock::now();
+}
+
+milliseconds toMilliseconds(double seconds)
+{
+  return milliseconds((long) (seconds * 1e3));
+}
+
+void TimeController::waitUntil(double simulationTime) const 
+{
+  auto realTime = sim_start_ + toMilliseconds(simulationTime / playbackSpeed_);
+  std::this_thread::sleep_until(realTime);
+}
+
+double TimeController::timeUntilNextFrame() const 
+{
+  return playbackSpeed_ / fps_;
+}
+
 
 Simulation::Simulation(const ParticleSystem& ps, const TimeController& tc) :
   ps_(ps), tc_(tc)
--- src/simulation.hpp
+++ src/simulation.hpp
@@ -2,10 +2,27 @@
 #define SIMULATION_HPP
 
 #include <vector>
+#include <chrono>
 #include "particle_system.hpp"
-#include "time_controller.hpp"
 
 using std::vector;
+using std::chrono::high_resolution_clock;
+using std::chrono::time_point;
+
+class TimeController {
+public:
+  TimeController(int targetFps, double playbackSpeed);
+  TimeController(int targetFps);
+
+  void setStartOfSimulation();
+  void waitUntil(double simulationTime) const;
+  double timeUntilNextFrame() const;
+
+private:
+  time_point<high_resolution_clock> sim_start_;
+  const int fps_;
+  const double playbackSpeed_;  
+};
 
 class Simulation {
 public:
