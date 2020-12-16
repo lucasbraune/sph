a959d4d9c4005eba01cabd5daefd9ebef09f3bf8
Lucas Braune <lucasbraune@gmail.com>
2020-12-16 14:08:38 -0300

    Inline operations with Vec2d

--- src/lib/util.cpp
+++ src/lib/util.cpp
@@ -5,62 +5,6 @@ using std::vector;
 
 namespace sph {
 
-Vec2d& operator+=(Vec2d& a, const Vec2d& b)
-{
-  a[0] += b[0];
-  a[1] += b[1];
-  return a;
-}
-
-Vec2d& operator-=(Vec2d& a, const Vec2d& b)
-{
-  a[0] -= b[0];
-  a[1] -= b[1];
-  return a;
-}
-
-Vec2d& operator*=(Vec2d& a, const double lambda)
-{
-  a[0] *= lambda;
-  a[1] *= lambda;
-  return a;
-}
-
-Vec2d& operator/=(Vec2d& a, const double lambda)
-{
-  return a *= 1.0 / lambda;
-}
-
-Vec2d operator+(Vec2d a, const Vec2d& b)
-{
-  return a += b;
-}
-
-Vec2d operator-(Vec2d a, const Vec2d& b)
-{
-  return a -= b;
-}
-
-Vec2d operator*(const double lambda, Vec2d a)
-{
-  return a *= lambda;
-}
-
-Vec2d operator*(Vec2d a, const double lambda)
-{
-  return a *= lambda;
-}
-
-Vec2d operator/(Vec2d a, const double lambda)
-{
-  return a /= lambda;
-}
-
-double operator*(const Vec2d& a, const Vec2d& b)
-{
-  return a[0] * b[0] + a[1] * b[1];
-}
-
 double norm(const Vec2d& x)
 {
   return hypot(x[0], x[1]);
@@ -74,26 +18,6 @@ double dist(const Vec2d& x, const Vec2d& y)
 Vec2d unit(const Vec2d& x)
 {
   return x / norm(x);
-}
-
-Vec2d project(const Vec2d& x, const Vec2d& unitNormal)
-{
-  return (x * unitNormal) * unitNormal;
-}
-
-double Rectangle::width() const
-{
-  return xmax - xmin;
-}
-
-double Rectangle::height() const
-{
-  return ymax - ymin;
-}
-
-Vec2d Rectangle::bottomLeft() const
-{
-  return Vec2d{xmin, ymin};
 }
 
 } // end namespace sph
--- src/lib/util.hpp
+++ src/lib/util.hpp
@@ -10,27 +10,48 @@ using Vec2d = std::array<double, 2>;
 
 constexpr Vec2d ZERO_VECTOR{0.0, 0.0};
 
-Vec2d& operator+=(Vec2d& a, const Vec2d& b);
-Vec2d& operator-=(Vec2d& a, const Vec2d& b);
-Vec2d& operator*=(Vec2d& a, double lambda);
-Vec2d& operator/=(Vec2d& a, double lambda);
+inline Vec2d& operator+=(Vec2d& a, const Vec2d& b)
+{
+  a[0] += b[0];
+  a[1] += b[1];
+  return a;
+}
 
-Vec2d operator+(Vec2d a, const Vec2d& b);
-Vec2d operator-(Vec2d a, const Vec2d& b);
-Vec2d operator*(double lambda, Vec2d a);
-Vec2d operator*(Vec2d a, double lambda);
-Vec2d operator/(Vec2d a, double lambda);
+inline Vec2d& operator-=(Vec2d& a, const Vec2d& b)
+{
+  a[0] -= b[0];
+  a[1] -= b[1];
+  return a;
+}
 
-double operator*(const Vec2d& a, const Vec2d& b);
+inline Vec2d& operator*=(Vec2d& a, const double lambda)
+{
+  a[0] *= lambda;
+  a[1] *= lambda;
+  return a;
+}
+
+inline Vec2d& operator/=(Vec2d& a, double lambda) { return a *= 1.0 / lambda; }
+inline Vec2d operator+(Vec2d a, const Vec2d& b) { return a += b; }
+inline Vec2d operator-(Vec2d a, const Vec2d& b) { return a -= b; }
+inline Vec2d operator*(double lambda, Vec2d a) { return a *= lambda; }
+inline Vec2d operator*(Vec2d a, double lambda) { return a *= lambda; }
+inline Vec2d operator/(Vec2d a, double lambda) { return a /= lambda; }
+
+inline double operator*(const Vec2d& a, const Vec2d& b) { return a[0] * b[0] + a[1] * b[1]; }
+inline Vec2d project(const Vec2d& x, const Vec2d& unitNormal)
+{
+  return (x * unitNormal) * unitNormal;
+}
+
 double norm(const Vec2d& x);
 double dist(const Vec2d& x, const Vec2d& y);
 Vec2d unit(const Vec2d& x);
-Vec2d project(const Vec2d& x, const Vec2d& unitNormal);
 
 struct Rectangle {
-  double width() const;
-  double height() const;
-  Vec2d bottomLeft() const;
+  double width() const { return xmax - xmin; }
+  double height() const { return ymax - ymin; }
+  Vec2d bottomLeft() const { return Vec2d{xmin, ymin}; }
   const double xmin, ymin, xmax, ymax;
 };
 
